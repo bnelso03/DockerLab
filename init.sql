06537bb72993e182d975ba41896a012ec600d49a
Brayden Nelson <bnelso03@uafs.edu>
2025-04-16 17:15:03 -0500

    Update

---
+++ init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Students (
+	studentID int,
+	lastName varchar(255),
+	lirstName varchar(255)
+);
+
+INSERT INTO Students VALUES (1, "Diaz", "Andrew");
+INSERT INTO Students VALUES (2, "Nelson", "Brayden");
+INSERT INTO Students VALUES (3, "Collind", "Gerry");
